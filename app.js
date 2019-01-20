1e88e35fdc1a36591896a49fd5145b830b8d4461
andela-sakande <surajudeen.akande@andela.com>
2019-01-20 20:47:24 +0100

    complete todo

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 import chalk from 'chalk';
+import Joi from 'joi';
 import db from './db';
 
 const app = express();
@@ -9,6 +10,10 @@ app.use(bodyParser.json());
 const PORT = 3000;
 
 const collection = 'todo';
+
+const schema = Joi.object().keys({
+  todo: Joi.string().required(),
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './../index.html'));
@@ -35,12 +40,26 @@ app.put('/:id', (req, res) => {
       });
 });
 
-app.post('/', (req, res) => {
+app.post('/', (req, res, next) => {
   const userInput = req.body;
-  db.getDB().collection(collection).insertOne(userInput, (err, result) => {
-    if (err) console.log(err);
-    else {
-      res.json({ result, document: result.ops[0] });
+
+  Joi.validate(userInput, schema, (err, result) => {
+    if (err) {
+      const error = new Error('Invalid input');
+      error.status = 400;
+      next(error);
+    } else {
+      db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+        if (err) {
+          const error = new Error('Failed to insert');
+          error.status = 400;
+          next(error);
+        } else {
+          res.json({
+            result, document: result.ops[0], msg: 'Successfully Inserted', error: null,
+          });
+        }
+      });
     }
   });
 });
@@ -54,6 +73,14 @@ app.delete('/:id', (req, res) => {
         res.json(result);
       }
     });
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status).json({
+    error: {
+      msg: err.message,
+    },
+  });
 });
 
 db.connect((err) => {
